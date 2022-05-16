38edd4351418f1be752010a4aca508fc9a21261e
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-05-16 09:22:43 -0300

    Criação de um componente funcional

---
+++ src/components/Primeiro.jsx
@@ -0,0 +1,8 @@
+export default function Primeiro(){
+    //É UM COMPONENTE FUNCIONAL
+    return(
+        <>
+            <h1>Primeiro Componente</h1>
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Primeiro from './components/Primeiro';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Primeiro/>
   </React.StrictMode>
 );
 

f1ab90b9939b3aae07f30db9f847955ba581ad33
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-05-16 10:36:18 -0300

    Componentes de Classe - Parte II

--- src/components/Saudacoes.jsx
+++ src/components/Saudacoes.jsx
@@ -5,6 +5,12 @@ export default class Saudacao extends Component{
         tipo: "Fala",
         nome: "Pedro"
     }
+
+    constructor(props){
+        super(props)
+        this.setTipo = this.setTipo.bind(this)
+    }
+
     setTipo(e){
         //console.log(e.target.value)
         this.setState(
@@ -19,8 +25,8 @@ export default class Saudacao extends Component{
             <div>
                 <h1> {tipo} {nome}!</h1>
                 <hr/>
-                <input type="text" placeholder='Tipo...' value={tipo} onChange={e => this.setTipo(e)} />
-                <input type="text" placeholder='Nome...' value={nome} />
+                <input type="text" placeholder='Tipo...' value={tipo} onChange={e => this.setTipo} />
+                <input type="text" placeholder='Nome...' value={nome} onChange={e => this.setNome(e)} />
             </div>
         )
     }

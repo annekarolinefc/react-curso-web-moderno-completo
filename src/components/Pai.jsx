9273c4b378cf2b7069b0ee6681dbd0a00ab08dfc
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-05-16 11:22:51 -0300

    Componente pai -> utilização de outra maneira

--- src/components/Pai.jsx
+++ src/components/Pai.jsx
@@ -2,12 +2,19 @@ import Filho from './Filho'
 export default props => 
     <>
         <h1> { props.nome } { props.sobrenome }</h1>
+        <h3>Filhos</h3>
+        
+        {/* SE EU REMOVER MEUS FILHOS E COLOCAR NO ARQUIVO RAIZ
+        DEVO INSERIR O SEGUINTE CÓDIGO:
+        {props.children}
+        */}
+
         <Filho nome="Greice Kelly" sobrenome="Fortunado do Carmo"/>
         <Filho nome="Luiz Fernando" sobrenome="Fortunado do Carmo"/>
         <Filho nome="Anne Karoline" sobrenome="Fortunado do Carmo"/>
 
         {/* PROPRIEDADE SPREAD */}
-        
+
         {/*MESMA PROPRIEDADE DO PAI*/}
         <Filho {...props} />
         {/* APROVEITANDO SOBRENOME */}
